536938cdc4cf025518657d5b24bdd6cad1a1cd30
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-06-01 03:28:39 +0530

    Added new features

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ import './auth.js';
 import './ui.js';
 
 import { initSearch } from './views/searchInit.js';
+import './views/routeEffects.js';
+import './views/scrollTopButton.js';
+import './views/darkMode.js';
 
 initRouter();
 initSearch();
-
-
